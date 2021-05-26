f59eec1d4876e6d6f8d727e065ce65d1c2ffd76d
AnnaAndropova <raduga_a@bk.ru>
2021-05-26 13:49:14 +0300

    Fix db

--- php/obj/Database.php
+++ php/obj/Database.php
@@ -20,7 +20,7 @@ class Database
 
         $this->conn = null;
 
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name."charset=utf8;", $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name.";charset=utf8;", $this->username, $this->password);
 
         return $this->conn;
     }
