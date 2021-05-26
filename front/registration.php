ae2d8a243873f02dab3e3f583fa1defc73b6073e
AnnaAndropova <raduga_a@bk.ru>
2021-05-26 13:59:45 +0300

    Fix title

--- front/registration.php
+++ front/registration.php
@@ -17,7 +17,7 @@ $allAddress = $address->getAll();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Воронеж. Коммунальные услуги</title>
+    <title>ВКУ</title>
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/fonts.css">
     <link rel="stylesheet" href="css/style.css">
